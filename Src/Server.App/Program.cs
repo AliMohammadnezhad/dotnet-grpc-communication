e60c5d02b2c34cb283e717c1f55b678f287ea403
Ali Mohammadnejad <a.mohammadnejad@dotin.ir>
2024-04-02 12:03:30 +0330

    implement Grpc Server

--- Src/Server.App/Program.cs
+++ Src/Server.App/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Grpc.Core;
+using Server.App.Services;
 
-Console.WriteLine("Hello, World!");
+var server = new Grpc.Core.Server
+{
+    Services = { StreamService.BindService(new StreamServiceImplementation()) },
+    Ports = { new ServerPort("localhost", 50051, ServerCredentials.Insecure) }
+};
+server.Start();
+
+Console.WriteLine("Server listening on port 50051...");
+await Task.Delay(-1);
+
+await server.ShutdownAsync();
