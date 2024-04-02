20da170d793a8478290c082a4fdeafff6a601d4b
Ali Mohammadnejad <a.mohammadnejad@dotin.ir>
2024-04-02 12:17:35 +0330

    Implement Client Side Services

--- Src/Client.App/Program.cs
+++ Src/Client.App/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Grpc.Core;
 
-Console.WriteLine("Hello, World!");
+try
+{
+    var channel = new Channel("0.0.0.0", ChannelCredentials.Insecure);
+    var client = new StreamService.StreamServiceClient(channel);
+
+    var streamingCall = client.StreamCharacters(new Google.Protobuf.WellKnownTypes.Empty());
+
+    await foreach (var response in streamingCall.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine(response.Character);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Failed to connect to server: {ex.Message}");
+}
+
+Console.WriteLine("Connection closed.");
